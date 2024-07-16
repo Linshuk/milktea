57d0b68fa862179a2e7c0200dd689482b83a6702
TEA\Administrator <18559802120@163.com>
2024-07-16 16:50:30 +0800

    更新内容： 1、分类完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,141 @@
 // index.js
-
-const my = require('../../utils/extendApi')
-// import toast from '../../utils/extendApi'
-
+const http = require('../../utils/http')
+const config = require('../../utils/config')
 Page({
+  data: {
+    
+    indexImgs: [], // 轮播图数据
+    news: [], // 公告列表
+    itemList: [  // 四个小标签的数据
+      {
+        id:1,
+        imgUrl: "../../images/icon/newProd.png",
+        tagName: "新品推荐",
+        navigateUrl: ""
+      },
+      {
+        id:2,
+        imgUrl: "../../images/icon/timePrice.png",
+        tagName: "限时特惠",
+        navigateUrl: ""
+      },
+      {
+        id:3,
+        imgUrl: "../../images/icon/neweveryday.png",
+        tagName: "每日疯抢",
+        navigateUrl: ""
+      },
+      {
+        id:4,
+        imgUrl: "../../images/icon/newprods.png",
+        tagName: "领优惠券",
+        navigateUrl: ""
+      }
+    ],
+
+    tagList: []
+
+
+  },
 
   onLoad(){
-    my.toast()
+    // this.getAllData()
+  },
 
-    // my.modal()
+  getAllData(){
+    this.getBannerList()
+    this.getNoticeList()
+    this.getTag()
+  },
 
 
-    // wx.showToast({
-    //   title: '这是一个toast',
-    //   icon: 'none'
-    // })
+  // 获取轮播图数据
+  getBannerList(){
+    var page = this
+    var params = {
+      url: config.bannerUrl,
+      method: "GET",
+      data: {},
+      callBack(res){
+        page.setData({
+          indexImgs: res
+        })
+      },
+      errCallBack(res){
+      }
+    }
+    http.request(params,true)
+  },
+
+
+  // 获取公告列表
+  getNoticeList(){
+    var page = this
+    var params = {
+      url: config.noticeList,
+      method: "GET",
+      data: {},
+      callBack(res){
+        console.log("callBack");
+        console.log(res.records);
+        page.setData({
+          news: res.records
+        })
+      },
+      errCallBack(res){
+      }
+    }
+    http.request(params,true)
+  },
+
+  getTag(){
+    var page = this
+    var params = {
+      url: config.prodTagList,
+      method: "GET",
+      data: {},
+      callBack(res){
+
+        page.setData({
+          tagList: res
+        })
+        for(var i = 0; i < res.length ; i++){
+          page.getTagProd(res[i].id,i)
+        }
+
+
+      },
+      errCallBack(res){
+      }
+    }
+    http.request(params,true)
+  },
+
+  getTagProd(id,index){
+    console.log(index);
+    var page = this
+    var params = {
+      url: config.prodListByTagId,
+      method: "GET",
+      data: {
+        tagId: id,
+        size: 6
+      },
+      callBack: (res) => {
+        var taglist = this.data.tagList;
+        taglist[index].prods = res.records
+
+        this.setData({
+          tagList: taglist,
+        });
+
+        console.log(this.data.tagList);
+      },
+      errCallBack(res){
+      }
+    }
+    http.request(params,true)
   }
+
 
 })
---
+++ utils/config.js
@@ -0,0 +1,21 @@
+var domain = "http://212.129.154.86:8086"; //统一接口域名，测试环境
+
+var bannerUrl = "/indexImgs"   // 首页轮播图
+var noticeList = "/shop/notice/noticeList"   // 公告列表
+var prodTagList = "/prod/tag/prodTagList"   // 公告列表
+var prodListByTagId = "/prod/prodListByTagId"   // 公告列表
+var categoryInfo = "/category/categoryInfo"   // 公告列表
+var pageProd = "/prod/pageProd"   // 分类列表右侧的数据
+
+
+
+
+
+
+exports.domain = domain;
+exports.bannerUrl = bannerUrl;
+exports.noticeList = noticeList;
+exports.prodTagList = prodTagList;
+exports.prodListByTagId = prodListByTagId;
+exports.categoryInfo = categoryInfo;
+exports.pageProd = pageProd;
